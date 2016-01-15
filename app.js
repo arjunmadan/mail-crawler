edad7665d671acb634f5f440ce8e164c9904a56e
Arjun Madan <arjunmadan1992@gmail.com>
2016-01-15 00:15:09 -0500

    Get message IDs for all messages on a page

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-var Bluebird = require("bluebird");
-var Request  = Bluebird.promisify(require("request"));
+var Request  = require("request");
+var Cheerio  = require("cheerio");
 
-var baseURL = "http://" + process.argv[2] + ".markmail.org/search/?page=";
-var count = 1;
+var baseUrl = "http://" + process.argv[2] + ".markmail.org/search/?page=";
+var count   = 1;
+var IdList  = [];
 
-Request(baseURL + count)
-.then(function (result) {
-	console.log(result);
-})
-.catch(function (error) {
-	console.log(error);
-})
+function getEmailUrls(baseUrl, count) {
+	
+	Request(baseUrl + count, function (error, response, html) {
+		if(!error && response.statusCode == 200) {
+			var $ = Cheerio.load(html);
+			$("div.result").each(function(i, element){
+				var a = $(this);
+				//console.log(a['0'].attribs.id);
+				IdList.push(a['0'].attribs.id);
+				count++;
+    		});
+		}
+		console.log(IdList);
+	});
+	
 
+}
+
+getEmailUrls(baseUrl, count);
 
